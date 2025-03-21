348bb291d2eca4ff7e2ecaca765969660acced8a
E-v7 <evanapogue@gmail.com>
2025-03-20 22:15:18 -0400

    Starting point backend code added

---
+++ server/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const settings = require("./settings.json");
+const app = express();
+const port = 3000;
+
+const { getImagesFromDatabase } = require("./scripts/database");
+
+app.get("/", (req, res) => {
+  res.send(
+    '<img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmedia1.tenor.com%2Fm%2FsW9GOo6TshcAAAAd%2Fevil-larry.gif&f=1&nofb=1&ipt=7925bf4ea209e6544363603f678cdc6ff9e99d12458d9f22c087df46176a2753&ipo=images" />'
+  );
+});
+
+// Example request localhost:port/get_images/1234/5678
+app.get("/get_images/:uid/:imgid?", async (req, res) => {
+  const user_id = req.params.uid;
+  const image_id = req.params.imgid; // This is optional and will be undefined if not provided
+
+  // Make call to database and get images then return files to user
+  const image = await getImagesFromDatabase(user_id, image_id);
+  if (!image) {
+    return res.status(404);
+  }
+
+  res.send(
+    `I have no image to send you right now but your user id is ${user_id} and you requested ${image_id}`
+  );
+});
+
+app.listen(port, () => {
+  console.log(`Express server running on http://localhost:${port}`);
+});
