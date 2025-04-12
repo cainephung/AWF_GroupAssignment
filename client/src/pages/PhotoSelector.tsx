cf9e96c5c88e76ac026e948e4d2999bc5028ed13
nguyenb12 <nguyennbrian123@gmail.com>
2025-04-11 23:24:35 -0400

    added a dummy photo list + click to select using useState

--- client/src/pages/PhotoSelector.tsx
+++ client/src/pages/PhotoSelector.tsx
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
 
-export default function PhotoSelector() {
-    return <div>Photo Selector Page</div>;
-}
-// Add photo array
+// Array photo
 const photos = [
     "Photo1", "Photo2", "Photo3", "Photo4", "Photo5",
     "Photo6", "Photo7", "Photo8", "Photo9", "Photo10"
 ];
 
-const [selected, setSelected] = useState("Photo1");
+export default function PhotoSelector() {
+    const [selected, setSelected] = useState("Photo1");
+
+    return (
+        <div>
+            {photos.map((photo) => (
+                <div key={photo} onClick={() => setSelected(photo)}>
+                    {photo}
+                </div>
+            ))}
+        </div>
+    );
+}
