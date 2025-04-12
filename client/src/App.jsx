bf690a996306b1bf0ae1630f7b9a98abf36f837b
nguyenb12 <nguyennbrian123@gmail.com>
2025-04-11 22:42:55 -0400

    modified App.jsx to route to the photos page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,8 @@ import RegisterPage from "./pages/RegisterPage";
 import SignInPage from "./pages/SignInPage";
 import ForgotPasswordPage from "./pages/ForgotPassword";
 import Header from "./assets/components/header";
+import PhotoSelector from "./pages/PhotoSelector";
+
 function App() {
   return (
     <Router>
@@ -11,6 +13,7 @@ function App() {
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/signin" element={<SignInPage />} />
         <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+        <Route path="/photos" element={<PhotoSelector />} />
       </Routes>
     </Router>
   );
