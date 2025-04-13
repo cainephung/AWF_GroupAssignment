b47cd0d9ccd0b13a65a90ecb20bec5564d4d6703
nguyenb12 <nguyennbrian123@gmail.com>
2025-04-13 00:56:08 -0400

    fix some changes with
    merge conflicts
    
    As well as updated css style so photopage/album page is centered

--- client/src/pages/AlbumPage.tsx
+++ client/src/pages/AlbumPage.tsx
@@ -1,32 +1,33 @@
-// imports 
 import React, { useState } from "react";
 import "../styles/global.css";
 
-// Dummy albums for demonstration will later be replaced with actually albums
+// Dummy albums for demonstration
 const photos = ["Album1", "Album2", "Album3", "Album4", "Album5"];
 
 export default function Albums() {
 
-    // State to track which album is currently selected
+    // State to track which albums is currently selected
     const [selected, setSelected] = useState("Album1");
 
     return (
         <div className="auth-container">
-            <div className="grid">
-                {photos.map((photo) => (
-                    <div
-                        key={photo}
-                        className={`box ${selected === photo ? "selected" : ""}`}
-                        onClick={() => setSelected(photo)}
-                    >
-                        <span className="album-text">{photo}</span>
-                    </div>
-                ))}
-            </div>
+            <div className="center-wrapper">
+                <div className="grid">
+                    {photos.map((photo) => (
+                        <div
+                            key={photo}
+                            className={`box ${selected === photo ? "selected" : ""}`}
+                            onClick={() => setSelected(photo)}
+                        >
+                            <span className="album-text">{photo}</span>
+                        </div>
+                    ))}
+                </div>
 
-            <div className="button-row">
-                <button className="primary-btn">Create</button>
-                <button className="primary-btn">Share</button>
+                <div className="button-row">
+                    <button className="primary-btn">Create</button>
+                    <button className="primary-btn">Share</button>
+                </div>
             </div>
         </div>
     );
