ab23826707ab7291f5e198e0b338aceb1ef3020a
Caine Phung <hphung3324@conestogac.on.ca>
2025-04-16 17:51:09 -0400

    Revert "Merge branch 'tags' into Demo_version"
    This reverts commit f4fe6ab3c6d78d2afd2ae4a616e3509d39360c98, reversing
    changes made to eecd3ff078453d1e02be54e4f5997f1a7aea9689.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./assets/components/header";
 import RegisterPage from "./pages/RegisterPage";
@@ -7,10 +8,10 @@ import HomePage from "./pages/HomePage";
 import Photos from "./pages/PhotoPage";
 import Albums from "./pages/AlbumPage";
 import CreateAlbumPage from "./pages/CreateAlbumPage";
-import AlbumView from "./pages/AlbumView";
-// Protected Route
+import AlbumView from "./pages/AlbumView"
+// Protected Route 
 import ProtectedRoute from "./assets/components/ProtectedRoute";
-import DisplayPhoto from "./pages/DisplayPhoto";
+
 
 function App() {
   return (
@@ -23,8 +24,9 @@ function App() {
         <Route path="/photos" element={<Photos />} />
         <Route path="/albums" element={<Albums />} />
         <Route path="/album/:albumName" element={<AlbumView />} />
-        <Route path="/photo/:name" element={<DisplayPhoto />} />
+
         <Route path="/create" element={<CreateAlbumPage />} />
+
 
         <Route
           path="/"
